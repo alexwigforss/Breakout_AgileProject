3c61f2878618dd691d596538b3574b88531e0824
alexander wigforss <alex.wigforss@gmail.com>
2023-04-17 15:47:38 +0200

    GamBlocks

---
+++ GameBoard.cs
@@ -0,0 +1,21 @@
+﻿namespace Breakout
+{
+    public static class GameBoard
+    {
+        static int width = 80;
+        static int height = 40;
+        static V2 topLeftCorner = new V2(1, 1);
+        static V2 bottomRightCorner = new V2(79, 39);
+
+        static V2 topLeftBrickZoneCorner = new V2(1, 5);
+        static V2 bottomRightBrickZoneCorner = new V2(79, 19);
+
+        public static int Width { get => width; set => width = value; }
+        public static int Height { get => height; set => height = value; }
+        public static V2 TopLeftCorner { get => topLeftCorner; set => topLeftCorner = value; }
+        public static V2 BottomRightCorner { get => bottomRightCorner; set => bottomRightCorner = value; }
+        public static V2 TopLeftBrickZoneCorner { get => topLeftBrickZoneCorner; set => topLeftBrickZoneCorner = value; }
+        public static V2 BottomRightBrickZoneCorner { get => bottomRightBrickZoneCorner; set => bottomRightBrickZoneCorner = value; }
+
+    }
+}
