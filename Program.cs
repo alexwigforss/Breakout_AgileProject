6997f322a991121e751983ff5bd4dc890353e4e7
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-04-17 11:02:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Breakout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+        }
+    }
+}
